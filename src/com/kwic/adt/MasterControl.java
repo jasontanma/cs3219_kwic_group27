3d7f11409fc3b8e07e169b013deb2eb88c569319
Kyle Leong Wei Ming <kylelwm@gmail.com>
2016-08-24 21:04:19 +0800

    Add option for file input

--- src/com/kwic/adt/MasterControl.java
+++ src/com/kwic/adt/MasterControl.java
@@ -13,6 +13,7 @@ public class MasterControl {
     public static void main(String[] args) {
 
         Interaction.displayWelcomeMessage();
+        Interaction.getInputMethod();
         ArrayList<Line> lines = Interaction.getLines();
         ArrayList<String> ignoredWords = Interaction.getIgnoredWords();
 
